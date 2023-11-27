6bc5c360bd55e10fdcb120c3b2687629aa6ad61b
MohamedAdel <adel762dp47@gmail.com>
2023-11-27 14:09:50 -0800

    message

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+x = "Holberton"
+a = x[:3]
+b = x[-2:]
+c = x[1:-1]
+print("First 3 letters: {}".format(a))
+print("Last 2 letters: {}".format(b))
+print("Middle word: {}".format(c))
